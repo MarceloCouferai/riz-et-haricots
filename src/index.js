537c52fc97a8028fdf27b412fa2b951874b4ec57
Marcelo <mcouferai@gmail.com>
2024-08-02 17:51:08 -0300

    Iniciando site

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+const header = document.querySelector("#header");
+
+hamburger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+console.log(hamburger);
+console.log(nav);
