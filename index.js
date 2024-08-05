3dc8a246aa13e7e470d9ce62f2b438b2e1c7842e
Marcelo <mcouferai@gmail.com>
2024-08-05 16:40:08 -0300

    criando json para registro de pratos

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+// MENU HAMBURGUER
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 const header = document.querySelector("#header");
-const optionHamburger = document.querySelector(".option-hamburger")
+const optionHamburger = document.querySelector(".option-hamburger");
 
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("active");
@@ -11,4 +12,12 @@ optionHamburger.addEventListener("click", () => {
     nav.classList.toggle("active");
 });
 
+
+//ADICIONAR CARDS
+const pratoPrincipal = [
+    {nome: 'Prato 1', content: 'teste', preco: 23.99}
+]
+
+
+// Animações
 AOS.init();
